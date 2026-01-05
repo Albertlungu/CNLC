a2691e554cc8d8ec1e96482513e036f49255045a
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-01-04 16:12:21 -1000

    FEAT: Complete business json population pipeline

--- scripts/overpass_api.py
+++ scripts/overpass_api.py
@@ -4,7 +4,7 @@ from typing import Any
 def fetch_businesses(
         city:str,
         debug=False
-        ) -> tuple[str, requests.Response, Any]:
+        ) -> tuple[str, requests.Response, dict]:
     """
     Gets businesses using requests library and OverpassAPI.
 
@@ -38,3 +38,49 @@ def fetch_businesses(
         # First 500 chars
 
     return query, response, data
+
+def compose_json(
+        data:dict
+        ) -> list[dict]:
+    """
+    Uses the data from the fetch_businesses() function to display relevant information about the
+    business.
+
+    Args:
+        data (dict): Data from fetch_businesses function.
+
+    Returns:
+        list[dict]: All business information as a list of dictionaries. To be compounded into a json
+                    file.
+    """
+
+    businesses = []
+
+    for element in data['elements']:
+        tags = element.get('tags', {})
+
+        if 'name' not in tags:
+            continue
+
+        business = {
+            'id': element.get('id'),
+            'name': tags.get('name'),
+            'latitude': element.get('lat'),
+            'longitude': element.get('lon'),
+            'address': {
+                'street': tags.get('addr:street'),
+                'housenumber': tags.get('addr:housenumber'),
+                'city': tags.get('addr:city'),
+                'country': tags.get('addr:country'),
+                'postcode': tags.get('addr:postcode')
+            },
+            'phone': tags.get('phone') or tags.get('contact:phone'),
+            'website': tags.get('website') or tags.get('contact:website'),
+            'opening_hours': tags.get('opening_hours'),
+            'category': tags.get('shop') or tags.get('amenity') or tags.get('craft'),
+            'cuisine': tags.get('cuisine')
+        }
+
+        businesses.append(business)
+
+    return businesses
