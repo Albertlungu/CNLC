b6530ca31ba4e5639870a3b46029a7c05604d131
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-01-08 12:10:19 -0500

    FEAT: Add removing bookmark functionality

--- backend/core/bookmark_manager.py
+++ backend/core/bookmark_manager.py
@@ -46,3 +46,41 @@ def create_bookmarks(
 
     del users # Deleting everything but keeping the selected user
     loaded_user['bookmarks'].extend(bookmarks)
+
+def remove_bookmarks(
+        username:str,
+        bookmarks:int | list[int],
+        output_filepath:str="../data/users.json",
+        ) -> None:
+    """
+    Removes bookmarks from a user's data.
+
+    Args:
+        username (str): The user's unique username.
+        bookmarks (list): The bookmarks they want to add.
+        output_filepath (str, optional): _description_. Defaults to "../data/users.json".
+    """
+    if output_filepath != "":
+        project_root = Path(__file__).parent.parent.parent
+        output_filepath = str(project_root / 'data' / 'businesses.json')
+
+    if isinstance(bookmarks, int):
+        bookmarks = [bookmarks]
+
+
+    users = jh.load_users() # Loading all users in memory
+    loaded_user = None
+    for user in users:
+        if user['username'] == username:
+            loaded_user = user
+            break
+
+    if loaded_user is None:
+        raise ValueError(f"ERROR: Username does not exist.")
+
+    del users # Deleting everything but keeping the selected user
+
+    for bookmark in bookmarks:
+        if bookmark not in loaded_user['bookmarks']:
+            raise ValueError(f"ERROR: Bookmark {bookmark} does not exist.")
+        loaded_user['bookmarks'].remove(bookmark)
