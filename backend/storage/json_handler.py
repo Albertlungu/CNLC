25b9ac3332c16f3834672b147d2465d03345a709
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-01-08 12:01:28 -0500

    REFACTOR: Move create_bookmarks into the dedicated bookmark_manager file.

--- backend/storage/json_handler.py
+++ backend/storage/json_handler.py
@@ -79,37 +79,3 @@ def save_users(
 
     with open(output_filepath, io_type) as f:
         json.dump(users, f, indent=1)
-
-def save_bookmarks(
-        username:str,
-        bookmarks:int | list[int],
-        output_filepath:str="../data/users.json",
-        ) -> None:
-    """
-    Adds bookmarks to a user's data.
-
-    Args:
-        username (str): The user's unique username.
-        bookmarks (list): The bookmarks they want to add.
-        output_filepath (str, optional): _description_. Defaults to "../data/users.json".
-    """
-    if output_filepath != "":
-        project_root = Path(__file__).parent.parent.parent
-        output_filepath = str(project_root / 'data' / 'businesses.json')
-
-    if isinstance(bookmarks, int):
-        bookmarks = [bookmarks]
-
-
-    users = load_users() # Loading all users in memory
-    loaded_user = None
-    for user in users:
-        if user['username'] == username:
-            loaded_user = user
-            break
-
-    if loaded_user is None:
-        raise ValueError(f"ERROR: Username does not exist.")
-
-    del users # Deleting everything but keeping the selected user
-    loaded_user['bookmarks'].extend(bookmarks)
