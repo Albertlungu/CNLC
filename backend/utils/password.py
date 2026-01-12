f45b018c78ec8d2e8e63b21966cec024fde93541
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-01-12 16:14:53 -0500

    CHORE: Fix formatting

--- backend/utils/password.py
+++ backend/utils/password.py
@@ -6,20 +6,22 @@ Password utils to make encryption protection for users.
 
 import bcrypt
 
-def hash_password(password:str) -> str:
+
+def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed.decode('utf-8')
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
-        plain_password.encode('utf-8'),
-        hashed_password.encode('utf-8')
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
 
 
 def main():
     return hash_password("")
 
+
 if __name__ == "__main__":
-   print(main())
+    print(main())
