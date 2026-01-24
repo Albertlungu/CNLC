9f75a4194835afad199273539e9d54da98e88713
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2026-01-23 22:09:36 -0500

    FEAT: Connect frontend and backend for reviews, making sure there is a separate page where users can view more business details and add reviews.

--- run_server.py
+++ run_server.py
@@ -15,4 +15,4 @@ sys.path.insert(0, str(project_root))
 from backend.api.server import app
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5001)
